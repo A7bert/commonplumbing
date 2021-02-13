8672a50d723fdfdc3b5bc647a241c30fbf1f3f4f
Albert Zhang <zzy0064@gmail.com>
2021-02-13 20:15:55 +1300

    outlines

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,45 @@
-console.log("hello world");
+const hamburger = document.querySelector('#btn-menu');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+hamburger.addEventListener('click', function(){
+  console.log('click hamburger');
+
+  if(header.classList.contains('open')){ // close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+    
+  }
+  else { // Open Hamburger Menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+
+  }  
+});
+
+const links = document.querySelectorAll(".menu-links a");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
